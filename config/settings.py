a19ed0857abee51730decd2e2a44bc04ae0fbbf0
Shima Ghezelbash <shimaghezelbash1986@gmail.com>
2026-02-05 18:37:14 +0330

    add custom user

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'accounts',
+
 ]
 
 MIDDLEWARE = [
@@ -109,3 +111,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+# accounts config
+AUTH_USER_MODEL = 'accounts.CustomUser'
